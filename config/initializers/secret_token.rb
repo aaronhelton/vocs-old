de753ad57d9e89563ad7ec7c9cfa22be73fbf61f
Aaron Helton <mariusagricola@gmail.com>
2013-09-23 09:21:44 -0400

    Initialize vocs repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vocs::Application.config.secret_key_base = '589ab72b0a4ebc655f365649cf0282d5340eeb0592cb39d673af01db299a77726186f0544bb484ed05e8521c42d7bf4398288e30128661a3ee33b1813ee641fa'
